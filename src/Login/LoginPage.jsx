502ff637bf998a72e0eb966dc670c81b71e84cff
Bryan Baker <bryanpbaker@gmail.com>
2017-11-22 21:05:27 -0600

    get user from api

--- src/Login/LoginPage.jsx
+++ src/Login/LoginPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import FacebookLogin from 'react-facebook-login';
 
 const LoginPage = () => {
-  const facebookResponse = (response) => {
-    console.log(response);
+  const BASE_URL = 'http://localhost:5000/auth/facebook?access_token=';
+
+  const apiAuth = (fbResponse) => {
+    console.log(fbResponse.accessToken);
+    axios.post(`${BASE_URL}${fbResponse.accessToken}`)
+      .then((apiResponse) => {
+        console.log(apiResponse.data);
+      });
   };
 
   return (
@@ -11,7 +18,7 @@ const LoginPage = () => {
       Login Page
       <FacebookLogin
         appId="1013591492112556"
-        callback={facebookResponse}
+        callback={apiAuth}
       />
     </div>
   );
