8c795f3aeaeee16a3a9e2230f652ef75cca8dbc8
Bryan Baker <bryanpbaker@gmail.com>
2017-11-25 23:59:57 -0600

    refactor email auth form to functional

--- src/containers/Login/EmailLogin/EmailLogin.jsx
+++ src/containers/Login/EmailLogin/EmailLogin.jsx
@@ -1,32 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-class EmailLogin extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.props.handleSubmit;
-  }
-
-  render() {
-    return (
-      <div className="email-login">
-        <h3>Login with your email address</h3>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Field name="email" component="input" className="form-control" type="email" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Field name="password" component="input" className="form-control" type="password" />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+const EmailLogin = ({ handleSubmit }) => (
+  <div className="email-login">
+    <h3>Login with your email address</h3>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" className="form-control" type="email" />
       </div>
-    );
-  }
-}
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <Field name="password" component="input" className="form-control" type="password" />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  </div>
+);
 
 export default reduxForm({
   form: 'EmailLogin',
