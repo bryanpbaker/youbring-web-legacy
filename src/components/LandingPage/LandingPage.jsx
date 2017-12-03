06cc24253f009edb0bbc24b32c80f3a3deeff7c4
Bryan Baker <bryanpbaker@gmail.com>
2017-12-02 19:38:20 -0600

    start styling and layout of landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import { Jumbotron, Grid, Row, Col, Button } from 'react-bootstrap';
+import './LandingPage.styles.css';
 
+import LandingPageHeader from './LandingPageHeader';
 import Login from '../../containers/Login/Login';
 import CreateUser from '../../containers/CreateUser/CreateUser';
 
 const LandingPage = () => (
   <div className="landing-page">
-    Landing Page
-    <Login />
-    <CreateUser />
+    <LandingPageHeader />
+    <Jumbotron>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <h1>Welcome to YouBring!</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis quo nisi veniam eius eligendi!</p>
+            <p><Button bsStyle="secondary">Learn more</Button></p>
+          </Col>
+        </Row>
+      </Grid>
+    </Jumbotron>
+    <Grid fluid>
+      <Row>
+        <Col xs={12}>
+          <h2 className="subheading">
+            Lorem, ipsum dolor.
+          </h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <hr/>
+          <h1>Heading 1</h1>
+          <h2>Heading 2</h2>
+          <h3>Heading 3</h3>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 );
 
---
+++ src/config/variables.js
@@ -0,0 +1,12 @@
+export const Colors = {
+  white: '#FFFFFF',
+  orange: '#F2784B',
+  navy: '#22313F',
+  lightGray: '#4A4A4A',
+  beige: '#BDC3C7',
+  red: '#D91E18',
+};
+
+export const Fonts = {
+  primary: 'Arial',
+};
