38af6d8006db71cafb2520146ebb1b636a638a5f
Bryan Baker <bryanpbaker@gmail.com>
2018-02-25 21:27:49 -0600

    deleted old Events.jsx and Login.jsx

--- src/views/Events/Events.jsx
+++ src/views/Events/Events.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppNavbar from '../../containers/AppNavbar/AppNavbar';
 
 const Dashboard = () => (
   <div className="dashboard">
+    <AppNavbar />
     Dashboard!
     <div>
       <Link to="/">Landing Page</Link>
