6bb8005230b026fc87d7a99a75566110b103e1ab
Bryan Baker <bryanpbaker@gmail.com>
2018-03-01 15:28:57 -0600

    added delete icon to eventcard

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Col } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 
 import './EventCard.css';
 
 const EventCard = props => (
   <Col xs={12} sm={6} md={3} className="event-card-container">
     <div className="event-card">
+      <button className="delete-button">
+        <FontAwesome
+          name="trash"
+        />
+      </button>
       <Link className="event-name" to={props.path}>{props.name}</Link>
       <p>{props.date}</p>
       <Link className="view-more" to={props.path}>View Details</Link>
