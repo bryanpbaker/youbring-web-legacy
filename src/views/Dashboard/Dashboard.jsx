ab3910b6c62b833df6b4d1f48529b63bbe7db47d
Bryan Baker <bryanpbaker@gmail.com>
2018-03-01 17:33:13 -0600

    removed redundant components

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Grid, Row, Col } from 'react-bootstrap';
-import { authorizeUser } from '../../actions/auth.actions';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 import AppNavbar from '../../containers/AppNavbar/AppNavbar';
-import TitleSection from '../../components/TitleSection/TitleSection';
 import EventsList from '../../containers/EventsList/EventsList';
+import './Dashboard.styles.css';
 
-const Dashboard = () => (
-  <div className="dashboard">
-    <AppNavbar />
-    <TitleSection
-      title="Dashboard"
-      actions={[
-        {
-          title: 'Create New Event',
-          icon: 'plus-circle',
-          color: 'accent',
-        },
-      ]}
-    />
-    <Grid fluid>
-      <Row>
-        <Col xs={12}>
-          <EventsList />
-        </Col>
-      </Row>
-    </Grid>
-  </div>
-);
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <AppNavbar />
+      <Grid fluid>
+        <Row className="title-section">
+          <Col xs={12}>    
+            <h1>Dashboard</h1>
+            <div className="actions pull-right">
+              <Button className="action-button" bsStyle="primary">
+                <FontAwesome
+                  name="plus-circle"
+                  size="2x"
+                />
+                <span className="title">Create New Event</span>
+              </Button>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <EventsList />
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  )
+};
 
 export default Dashboard;
